61ea69561d9045c855f0c397e3b0a9c59724aa32
Bertrand Tourlonias <btourlonias@openstudio.fr>
2022-04-26 13:01:07 +0200

    WIP inital structure

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import Button from './components/Button/Button'
+
+function App() {
+    return (
+        <div>
+            <Button/>
+            <Button/>
+            <Button/>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/App/components/Button/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import style from './Button.module.css';
+
+const Button = () => {
+    return <button className={style.Button}>toto</button>
+}
+
+export default Button;
